61d1d35d0d9d7221d45b93a40b0237f893f5da2b
numeroab <renanfelipefranco@gmail.com>
2025-09-22 15:15:45 -0300

    mudanças pós-provas

--- routes/users.js
+++ routes/users.js
@@ -1,62 +1,15 @@
-const express = require('express');
+import {registrerUser, loginUser, getTodosDados, getDadoPorId, atualizaDado, deletaDado} from "../controllers/usersController.js"
+import { authenticateToken } from '../middleware/auth.js';
+import express from 'express';
+
+
 const router = express.Router();
-const database = require('../dados/database');
-router.post('/', async (req, res) => {
-    try {
-      const { nome, email, senha} = req.body;
-      
-      if (!nome || !email || !senha) {
-        return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
-      }
-      
-      const novoUsuario = await database.create(req.body);
-      res.status(201).json(novoUsuario);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  });
 
-  router.post('/', async (req, res) => {
-   
-  });
+router.post('/register', registrerUser);
+router.post('/login', loginUser);
+router.get('/users', authenticateToken, getTodosDados);
+router.get('/users/:id', authenticateToken, getDadoPorId);
+router.put('/users/:id', authenticateToken, atualizaDado);
+router.delete('/users/:id', authenticateToken, deletaDado);
 
-
-
-router.get('/', async (req, res) => {
-    try {
-      const usuarios = await database.getAll();
-      res.json(alunos);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
-  router.get('/:id', async (req, res) => {
-    try {
-      const usuario = await database.getById(req.params.id);
-      
-      if (usuario) {
-        res.json(usuario);
-      } else {
-        res.status(404).json({ error: 'Usuário não encontrado' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
-  router.delete('/:id', async (req, res) => {
-    try {
-      const alunoRemovido = await database.delete(req.params.id);
-      
-      if (alunoRemovido) {
-        res.json({ message: 'Usuario removido com sucesso', aluno: alunoRemovido });
-      } else {
-        res.status(404).json({ error: 'Usuario não encontrado' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  
-  module.exports = router;
+export { router };
